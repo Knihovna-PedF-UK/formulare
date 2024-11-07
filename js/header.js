dc4cd6dd48170ecce68f44b22afc23953cff3923
Michal Hoftich <michal.h21@gmail.com>
2024-11-07 16:46:45 +0100

    don't use header.js

--- js/header.js
+++ js/header.js
@@ -1,6 +1,7 @@
+// tenhle kód už nepoužíváme, hlavička je v tpl/header.html
 window.addEventListener("load", (event) => {
   const header = document.getElementById("header")
   if(header){
-    header.innerHTML = `<menu>//<a href="./">Domů</a>//<a href="./signatura2.html">Signatury 2</a></menu>`
+    header.innerHTML = ``
   }
 });
