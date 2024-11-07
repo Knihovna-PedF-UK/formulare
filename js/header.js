f2749980f2cd68981fb9002ab8e4d6ebd7a88d41
Michal Hoftich <michal.h21@gmail.com>
2024-11-07 16:47:03 +0100

    maybe even remove it

--- js/header.js
+++
@@ -1,7 +0,0 @@
-// tenhle kód už nepoužíváme, hlavička je v tpl/header.html
-window.addEventListener("load", (event) => {
-  const header = document.getElementById("header")
-  if(header){
-    header.innerHTML = ``
-  }
-});
