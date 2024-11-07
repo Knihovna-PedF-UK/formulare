f9fa47a2e69d10eff0ccfc6a8cee0f08fd5d9d2b
michal-h21 <michal-h21@users.noreply.github.com>
2024-11-07 16:58:41 +0000

    deploy: c0e68f21432537e28e5f97a27dab2bd47c2237fa

--- js/header.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", (event) => {
-  const header = document.getElementById("header")
-  if(header){
-    header.innerHTML = `<menu>//<a href="./">Domů</a>//<a href="./signatura2.html">Signatury 2</a></menu>`
-  }
-});
---
+++ js/signatury.js
@@ -0,0 +1,43 @@
+function extractSignatures(text) {
+    const signatures = [];
+    const lines = text.split('\n');  // Rozdělíme řetězec na jednotlivé řádky
+
+    lines.forEach(line => {
+        const match = line.match(/^[A-Za-z0-9ěščřžýáíéúůťďň.,\-/]+/);  // Najdeme signaturu na začátku řádku
+        if (match) {
+            signatures.push(match[0]);  // Přidáme nalezenou signaturu do pole
+        }
+    });
+
+    return signatures;
+}
+
+function displaySignaturesInTable(signatures, tableContainer) {
+
+    // Vytvoříme HTML strukturu tabulky
+    let tableHTML = '<table border="1"><tr><th>Signatura</th></tr>';
+
+    // Projdeme každou signaturu a přidáme ji jako řádek do tabulky
+    signatures.forEach(signature => {
+        tableHTML += `<tr><td>${signature}</td></tr>`;
+    });
+
+    tableHTML += '</table>';
+
+    // Vložíme HTML tabulky do elementu "tabulka"
+    tableContainer.innerHTML = tableHTML;
+}
+
+function signaturyCode(str){
+  return `\\stitky{${str}}`
+}
+
+function generateSignatury(dataArray, template){
+  // Zavoláme signaturyCode na každou položku v dataArray a spojíme výsledné řetězce
+  const content = dataArray.map(item => signaturyCode(item)).join("\n");
+  return template.replace('{{content}}', content);
+}
+module.exports = {
+  extractSignatures, 
+  signaturyCode
+}
