b4ea12eab897c44c55aa81ff9cfff9a5d4c21f19
michal-h21 <michal-h21@users.noreply.github.com>
2024-11-08 12:47:35 +0000

    deploy: 568873dcf5728b0d4e740a39e4177490606402a0

---
+++ js/carovekody.js
@@ -0,0 +1,40 @@
+
+function parseTable(text) {
+  if (text.trim() === '') {
+    return [];
+  }
+  const rows = text.split('\n');
+  return rows.map(row => row.split('\t').map(cell => cell.trim()));
+}
+
+function renderTable(dataArray, container) {
+  if (dataArray.length === 0) return; // Pokud je pole prázdné, nevykresluje
+
+  const table = document.createElement('table');
+  dataArray.forEach(rowData => {
+    if(rowData){
+      const tr = document.createElement('tr');
+      rowData.forEach(name => {
+        const td = document.createElement('td');
+        // td.setAttribute("class", name)
+        const cellData = name
+        td.textContent = cellData;
+        tr.appendChild(td);
+      });
+      table.appendChild(tr);
+    }
+  });
+  container.innerHTML = ''; // Vyprázdní obsah kontejneru
+  container.appendChild(table); // Přidá nově vytvořenou tabulku
+}
+
+function barcode(items){
+  return `\\carovykod{${items[0]}}{${items[1]}}%`
+}
+
+
+function generateBarcodes(dataArray, template){
+  // Zavoláme signaturyCode na každou položku v dataArray a spojíme výsledné řetězce
+  const content = dataArray.map(item => barcode(item)).join("\n");
+  return template.replace('{{content}}', content);
+}
