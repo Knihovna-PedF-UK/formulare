42895d8e74bc34af43055f16ed1e05495af475df
Michal Hoftich <michal.h21@gmail.com>
2024-12-04 15:33:45 +0100

    zakoduj binarni soubory

--- replace_tags.py
+++ replace_tags.py
@@ -1,6 +1,6 @@
 import os
 import re
-# from bs4 import BeautifulSoup
+import base64
 
 # Cesty k adresářům
 data_dir = "data"
@@ -11,6 +11,9 @@ os.makedirs(output_dir, exist_ok=True)
 
 # Pravidelný výraz pro hledání značek ${jmenosouboru}
 pattern = re.compile(r'\$\{([^}]+)\}')
+
+# Seznam přípon, jejichž obsah bude zakódován pomocí base64
+base64_extensions = ['.pdf', '.jpg', '.png']
 
 def replace_tags_in_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -24,8 +27,16 @@ def replace_tags_in_file(file_path):
 
         # Pokud odkazovaný soubor existuje, nahraď značku jeho obsahem
         if os.path.isfile(data_file_path):
-            with open(data_file_path, 'r', encoding='utf-8') as data_file:
+            with open(data_file_path, 'rb') as data_file:  # Otevřeme v binárním režimu pro base64
                 data_content = data_file.read()
+            
+            # Zakóduj obsah souboru, pokud má požadovanou příponu
+            _, ext = os.path.splitext(match)
+            if ext in base64_extensions:
+                data_content = base64.b64encode(data_content).decode('utf-8')
+            else:
+                data_content = data_content.decode('utf-8')
+            
             content = content.replace(f"${{{match}}}", data_content)
 
     # Ulož aktualizovaný soubor do složky out
